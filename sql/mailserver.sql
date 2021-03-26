3adfc75aaa46adf8b7e7ba6e6acf72ba56cdbd51
Moov Space <moovspace@gmail.com>
2021-03-26 21:37:39 +0100

    Mysql db

--- sql/mailserver.sql
+++ sql/mailserver.sql
@@ -1,108 +1,131 @@
--- MySQL dump 10.18  Distrib 10.3.27-MariaDB, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: mailserver
--- ------------------------------------------------------
--- Server version	10.3.27-MariaDB-0+deb10u1
+-- Host: localhost
+-- Czas generowania: 26 Mar 2021, 21:12
+-- Wersja serwera: 10.3.27-MariaDB-0+deb10u1
+-- Wersja PHP: 7.3.27-1~deb10u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `virtual_aliases`
+-- Baza danych: `mailserver`
+--
+CREATE DATABASE IF NOT EXISTS `mailserver` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mailserver`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `virtual_aliases`
 --
 
 DROP TABLE IF EXISTS `virtual_aliases`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `virtual_aliases` (
+CREATE TABLE IF NOT EXISTS `virtual_aliases` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `domain_id` int(11) NOT NULL,
   `source` varchar(100) NOT NULL,
   `destination` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `domain_id` (`domain_id`),
-  CONSTRAINT `virtual_aliases_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
+  KEY `domain_id` (`domain_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `virtual_aliases`
+-- Tabela Truncate przed wstawieniem `virtual_aliases`
 --
 
-LOCK TABLES `virtual_aliases` WRITE;
-/*!40000 ALTER TABLE `virtual_aliases` DISABLE KEYS */;
-INSERT INTO `virtual_aliases` VALUES (1,1,'alias@example.com','email1@example.com');
-/*!40000 ALTER TABLE `virtual_aliases` ENABLE KEYS */;
-UNLOCK TABLES;
+TRUNCATE TABLE `virtual_aliases`;
+--
+-- Zrzut danych tabeli `virtual_aliases`
+--
+
+INSERT INTO `virtual_aliases` (`id`, `domain_id`, `source`, `destination`) VALUES
+(1, 1, 'alias@example.com', 'email1@example.com');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `virtual_domains`
+-- Struktura tabeli dla tabeli `virtual_domains`
 --
 
 DROP TABLE IF EXISTS `virtual_domains`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `virtual_domains` (
+CREATE TABLE IF NOT EXISTS `virtual_domains` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `virtual_domains`
+-- Tabela Truncate przed wstawieniem `virtual_domains`
 --
 
-LOCK TABLES `virtual_domains` WRITE;
-/*!40000 ALTER TABLE `virtual_domains` DISABLE KEYS */;
-INSERT INTO `virtual_domains` VALUES (1,'example.com'),(2,'hostname.example.com'),(3,'hostname'),(4,'localhost.example.com');
-/*!40000 ALTER TABLE `virtual_domains` ENABLE KEYS */;
-UNLOCK TABLES;
+TRUNCATE TABLE `virtual_domains`;
+--
+-- Zrzut danych tabeli `virtual_domains`
+--
+
+INSERT INTO `virtual_domains` (`id`, `name`) VALUES
+(1, 'example.com'),
+(2, 'hostname.example.com'),
+(3, 'hostname'),
+(4, 'localhost.example.com');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `virtual_users`
+-- Struktura tabeli dla tabeli `virtual_users`
 --
 
 DROP TABLE IF EXISTS `virtual_users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `virtual_users` (
+CREATE TABLE IF NOT EXISTS `virtual_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `domain_id` int(11) NOT NULL,
   `password` varchar(106) NOT NULL,
   `email` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
-  KEY `domain_id` (`domain_id`),
-  CONSTRAINT `virtual_users_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
+  KEY `domain_id` (`domain_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `virtual_users`
+-- Tabela Truncate przed wstawieniem `virtual_users`
 --
 
-LOCK TABLES `virtual_users` WRITE;
-/*!40000 ALTER TABLE `virtual_users` DISABLE KEYS */;
-INSERT INTO `virtual_users` VALUES (1,1,'$6$2e49b99b554afbc7$GjUNxUBKX2lVlKvUIuAA.0fatLb2s.W4QGiY2VkTpJtGY7MB.B.tPJteDH296A8iKpXRkq6LHtoB2BTBofjXE.','email1@example.com'),(2,1,'$6$125f250cb68b8af2$/Hj1W2BrbUQqPk/YG5LR53shQ2icScjcfo78/d4q2WsrOCQ5IXe.Bk8IlT/6Bwm7WIjamp/2WGPhpZiIOoGj/1','email2@example.com');
-/*!40000 ALTER TABLE `virtual_users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+TRUNCATE TABLE `virtual_users`;
+--
+-- Zrzut danych tabeli `virtual_users`
+--
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+INSERT INTO `virtual_users` (`id`, `domain_id`, `password`, `email`) VALUES
+(1, 1, '$6$4676f225e2988d78$iMvMbrHsOKu0k8FSgKMxlv5fE6460vt/njtPLZJ2gfphxj6Fuo6xJBfB8VbKuVwERNtlzCxikGsA4EzwWnUZn1', 'email1@example.com'),
+(2, 1, '$6$29512b77bf1ca730$gOdhKcgihQ9dn7p15VDiDnZUa3hkv4eGFc2ye83UzUatbfWgDUZLubuXNol/Mn4ZbisyX4N3XnhVdq000UD6A0', 'email2@example.com');
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `virtual_aliases`
+--
+ALTER TABLE `virtual_aliases`
+  ADD CONSTRAINT `virtual_aliases_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `virtual_users`
+--
+ALTER TABLE `virtual_users`
+  ADD CONSTRAINT `virtual_users_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-03-26 11:23:49
